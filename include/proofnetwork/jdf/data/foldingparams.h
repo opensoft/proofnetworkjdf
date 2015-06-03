6694a68959e9beed9ba22911b5b36f90574f9bcd
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-03 12:38:23 +0300

    FoldingParams class implemented

--- include/proofnetwork/jdf/data/foldingparams.h
+++ include/proofnetwork/jdf/data/foldingparams.h
@@ -1,4 +1,45 @@
 #ifndef FOLDINGPARAMS_H
 #define FOLDINGPARAMS_H
 
+#include "proofnetwork/jdf/data/abstractresource.h"
+
+#include <QXmlStreamReader>
+
+namespace Proof {
+namespace Jdf {
+
+class FoldingParamsPrvate;
+class PROOF_NETWORK_JDF_EXPORT FoldingParams : public AbstractResource
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(FoldingParams)
+public:
+    QString foldCatalog() const;
+    // Describes the type of fold according to the folding catalog
+    // Value format is: " Fn-i " where “n” is the number of finished
+    // pages and “i” is either an integer, which identifies a particular
+    // fold or the letter "X" , which identifies a generic fold.
+    // E.g., "F6-2" describes a Z-fold of 6 finished pages, and "F6-X . "
+    // describes a generic fold with 6 finished pages.
+    // Reference: http://www.cip4.org/documents/jdf_specifications/html/Resources.html#0_FoldingParams
+    void setFoldCatalog(const QString &foldCatalog);
+
+    static FoldingParamsSP create();
+
+    static FoldingParamsSP fromJdf(QXmlStreamReader &xmlReader);
+    void toJdf(QXmlStreamWriter &jdfWriter);
+    static FoldingParamsSP defaultObject();
+
+signals:
+    void foldCatalogChanged(QString arg);
+
+protected:
+    explicit FoldingParams();
+
+};
+
+
+}
+}
+
 #endif // FOLDINGPARAMS_H
--- src/proofnetwork/jdf/data/foldingparams.cpp
+++ src/proofnetwork/jdf/data/foldingparams.cpp
@@ -1,4 +1,92 @@
 #ifndef FOLDINGPARAMS_CPP
 #define FOLDINGPARAMS_CPP
 
+#include "foldingparams.h"
+
+namespace Proof {
+namespace Jdf {
+
+class FoldingParamsPrivate : AbstractResourcePrivate
+{
+    Q_DECLARE_PUBLIC(FoldingParams)
+
+    void updateFrom(const Proof::NetworkDataEntitySP &other) override;
+
+    QString foldCatalog = "F2-1"; //without folding by default
+};
+
+} // namespace Jdf
+} // namespace Proof
+
+using namespace Proof::Jdf;
+
+QString FoldingParams::foldCatalog() const
+{
+    Q_D(const FoldingParams);
+    return d->foldCatalog;
+}
+
+void FoldingParams::setFoldCatalog(const QString &foldCatalog)
+{
+    Q_D(FoldingParams);
+    // TODO: verify foldCatalog
+    if (d->foldCatalog != foldCatalog) {
+        d->foldCatalog = foldCatalog;
+        emit foldCatalogChanged(d->foldCatalog);
+    }
+}
+
+FoldingParamsSP FoldingParams::create()
+{
+    FoldingParamsSP result(new FoldingParams());
+    result->d_func()->weakSelf = result.toWeakRef();
+    return result;
+}
+
+FoldingParamsSP FoldingParams::defaultObject()
+{
+    static FoldingParamsSP entity = create();
+    return entity;
+}
+
+FoldingParamsSP FoldingParams::fromJdf(QXmlStreamReader &xmlReader)
+{
+    FoldingParamsSP foldingParams = create();
+
+    while (!xmlReader.atEnd() && !xmlReader.hasError()) {
+        if (xmlReader.name() == "FoldingParams" && xmlReader.isStartElement() && !foldingParams->isFetched()) {
+            foldingParams->setFetched(true);
+            QXmlStreamAttributes attributes = xmlReader.attributes();
+            foldingParams->setFoldCatalog( attributes.value("FoldCatalog").toString());
+            AbstractResourceSP castedFoldingParams = qSharedPointerCast<AbstractResource>(foldingParams);
+            AbstractResource::fromJdf(xmlReader, castedFoldingParams);
+
+        } else if (xmlReader.isStartElement()) {
+            xmlReader.skipCurrentElement();
+        } else if (xmlReader.isEndElement()) {
+            break;
+        }
+        xmlReader.readNext();
+    }
+
+    return foldingParams;
+}
+
+void FoldingParams::toJdf(QXmlStreamWriter &jdfWriter)
+{
+    Q_D(FoldingParams);
+
+    jdfWriter.writeStartElement("FoldingParams");
+    jdfWriter.writeAttribute("FoldCatalog", d->foldCatalog);
+    AbstractResource::toJdf(jdfWriter);
+    jdfWriter.writeEndElement();
+}
+
+FoldingParams::FoldingParams()
+{
+    setResourceClass(ApiHelper::ParameterClass);
+}
+
+
+
 #endif // FOLDINGPARAMS_CPP
