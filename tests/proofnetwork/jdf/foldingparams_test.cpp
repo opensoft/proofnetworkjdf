5b8cfc3cb59684c98f523ef066369de1d313d50b
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-04 10:40:51 +0300

    Test files add to a project

---
+++ tests/proofnetwork/jdf/foldingparams_test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/test_global.h"
+
+#include <QXmlStreamReader>
+#include <QSignalSpy>
+#include <QFile>
+#include <QDebug>
+
+using namespace Proof;
+using namespace Proof::Jdf;
+using testing::Test;
+
+
