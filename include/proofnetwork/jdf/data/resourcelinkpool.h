48be32f9769a3e851548c9553b5e98b325f9590a
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-12 17:21:28 +0300

    ResourceLinkPool class added

---
+++ include/proofnetwork/jdf/data/resourcelinkpool.h
@@ -0,0 +1,54 @@
+#ifndef RESOURCELINKPOOL_H
+#define RESOURCELINKPOOL_H
+
+#include "proofnetwork/networkdataentity.h"
+#include "proofnetwork/jdf/data/qmlwrappers/resourcelinkpoolqmlwrapper.h"
+#include "proofnetwork/jdf/proofnetworkjdf_types.h"
+#include "proofnetwork/jdf/proofnetworkjdf_global.h"
+
+#include <QXmlStreamReader>
+#include <QList>
+
+namespace Proof {
+namespace Jdf {
+
+class ResourceLinkPoolPrivate;
+class ResourceLinkPool : public NetworkDataEntity
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(ResourceLinkPool)
+public:
+    QList<ComponentLinkSP> componentLinks() const;
+    CuttingParamsLinkSP cuttingParamsLink() const;
+    MediaLinkSP mediaLink() const;
+    LaminatingIntentLinkSP laminatingIntentLink() const;
+    FoldingParamsLinkSP foldingParamsLink() const;
+
+    void setComponentLinks(const QList<ComponentLinkSP> &componentLinks);
+    void setCuttingParamsLink(const CuttingParamsLinkSP &cuttingParams);
+    void setMediaLink(const MediaLinkSP &mediaLink);
+    void setLaminatingIntentLink(const LaminatingIntentLinkSP &laminatingIntent);
+    void setFoldingParamsLink(const FoldingParamsLinkSP &foldingParamsLink);
+
+    ResourceLinkPoolQmlWrapper *toQmlWrapper(QObject *parent = 0) const override;
+
+    static ResourceLinkPoolSP create();
+
+    static ResourceLinkPoolSP fromJdf(QXmlStreamReader &xmlReader);
+    void toJdf(QXmlStreamWriter &jdfWriter);
+    static ResourceLinkPoolSP defaultObject();
+
+signals:
+    void componentLinksChanged();
+    void cuttingParamsLinkChanged(const Proof::Jdf::CuttingParamsLinkSP &arg);
+    void mediaLinkChanged(const Proof::Jdf::MediaLinkSP &arg);
+    void laminatingIntentLinkChanged(const Proof::Jdf::LaminatingIntentLinkSP &arg);
+    void foldingParamsLinkChanged(const Proof::Jdf::FoldingParamsLinkSP &arg);
+
+protected:
+    explicit ResourceLinkPool();
+};
+
+}
+}
+#endif // RESOURCELINKPOOL_H
---
+++ src/proofnetwork/jdf/data/resourcelinkpool.cpp
@@ -0,0 +1,224 @@
+#include "resourcelinkpool.h"
+#include "proofnetwork/networkdataentity_p.h"
+#include "proofnetwork/jdf/data/componentlink.h"
+#include "proofnetwork/jdf/data/cuttingparamslink.h"
+#include "proofnetwork/jdf/data/foldingparamslink.h"
+#include "proofnetwork/jdf/data/laminatingintentlink.h"
+#include "proofnetwork/jdf/data/medialink.h"
+
+
+namespace Proof {
+namespace Jdf {
+
+class ResourceLinkPoolPrivate : public NetworkDataEntityPrivate
+{
+    Q_DECLARE_PUBLIC(ResourceLinkPool)
+
+    void updateFrom(const Proof::NetworkDataEntitySP &other) override;
+
+    QList<ComponentLinkSP> componentLinks;
+    CuttingParamsLinkSP cuttingParamsLink;
+    MediaLinkSP mediaLink;
+    LaminatingIntentLinkSP laminatingIntentLink;
+    FoldingParamsLinkSP foldingParamsLink;
+};
+
+}
+}
+
+using namespace Proof::Jdf;
+
+QList<ComponentLinkSP> ResourceLinkPool::componentLinks() const
+{
+    Q_D(const ResourceLinkPool);
+    return d->componentLinks;
+}
+
+CuttingParamsLinkSP ResourceLinkPool::cuttingParamsLink() const
+{
+    Q_D(const ResourceLinkPool);
+    return d->cuttingParamsLink;
+}
+
+MediaLinkSP ResourceLinkPool::mediaLink() const
+{
+    Q_D(const ResourceLinkPool);
+    return d->mediaLink;
+}
+
+LaminatingIntentLinkSP ResourceLinkPool::laminatingIntentLink() const
+{
+    Q_D(const ResourceLinkPool);
+    return d->laminatingIntentLink;
+}
+
+FoldingParamsLinkSP ResourceLinkPool::foldingParamsLink() const
+{
+    Q_D(const ResourceLinkPool);
+    return d->foldingParamsLink;
+}
+
+void ResourceLinkPool::setComponentLinks(const QList<ComponentLinkSP> &componentLinks)
+{
+    Q_D(ResourceLinkPool);
+
+    // TODO: check, that links changed
+    d->componentLinks = componentLinks;
+    emit componentLinksChanged();
+}
+
+void ResourceLinkPool::setCuttingParamsLink(const CuttingParamsLinkSP &cuttingParams)
+{
+    Q_D(ResourceLinkPool);
+    if (d->cuttingParamsLink != cuttingParams) {
+        d->cuttingParamsLink = cuttingParams;
+        emit cuttingParamsLinkChanged(d->cuttingParamsLink);
+    }
+}
+
+void ResourceLinkPool::setMediaLink(const MediaLinkSP &mediaLink)
+{
+    Q_D(ResourceLinkPool);
+    if (d->mediaLink != mediaLink) {
+        d->mediaLink = mediaLink;
+        emit mediaLinkChanged(d->mediaLink);
+    }
+}
+
+void ResourceLinkPool::setLaminatingIntentLink(const LaminatingIntentLinkSP &laminatingIntent)
+{
+    Q_D(ResourceLinkPool);
+    if (d->laminatingIntentLink != laminatingIntent) {
+        d->laminatingIntentLink = laminatingIntent;
+        emit laminatingIntentLinkChanged(d->laminatingIntentLink);
+    }
+}
+
+void ResourceLinkPool::setFoldingParamsLink(const FoldingParamsLinkSP &foldingParamsLink)
+{
+    Q_D(ResourceLinkPool);
+    if (d->foldingParamsLink != foldingParamsLink) {
+        d->foldingParamsLink = foldingParamsLink;
+        emit foldingParamsLinkChanged(d->foldingParamsLink);
+    }
+}
+
+ResourceLinkPoolQmlWrapper *ResourceLinkPool::toQmlWrapper(QObject *parent) const
+{
+    Q_D(const ResourceLinkPool);
+    ResourceLinkPoolSP castedSelf = qSharedPointerCast<ResourceLinkPool>(d->weakSelf);
+    Q_ASSERT(castedSelf);
+    return new ResourceLinkPoolQmlWrapper(castedSelf, parent);
+
+}
+
+ResourceLinkPoolSP ResourceLinkPool::create()
+{
+    ResourceLinkPoolSP result(new ResourceLinkPool());
+    result->d_func()->weakSelf = result.toWeakRef();
+    return result;
+}
+
+ResourceLinkPoolSP ResourceLinkPool::fromJdf(QXmlStreamReader &xmlReader)
+{
+    ResourceLinkPoolSP linkPool = create();
+
+    QList<ComponentLinkSP> components;
+    while (!xmlReader.atEnd() && !xmlReader.hasError()) {
+        if (xmlReader.name() == "ResourceLinkPool" && xmlReader.isStartElement() && !linkPool->isFetched()) {
+            linkPool->setFetched(true);
+        } else if (xmlReader.isStartElement()) {
+            if (xmlReader.name() == "MediaLink") {
+                MediaLinkSP media = MediaLink::fromJdf(xmlReader);
+                if (!media) {
+                    qCCritical(proofNetworkJdfDataLog) << "MediaLink not created.";
+                    return ResourceLinkPoolSP();
+                }
+                linkPool->setMediaLink(media);
+            } else if (xmlReader.name() == "LaminatingIntentLink") {
+                LaminatingIntentLinkSP laminatingIntent = LaminatingIntentLink::fromJdf(xmlReader);
+                if (!laminatingIntent) {
+                    qCCritical(proofNetworkJdfDataLog) << "LaminatingIntentLink not created.";
+                    return ResourceLinkPoolSP();
+                }
+                linkPool->setLaminatingIntentLink(laminatingIntent);
+            } else if (xmlReader.name() == "ComponentLink") {
+                ComponentLinkSP component = ComponentLink::fromJdf(xmlReader);
+                if (!component) {
+                    qCCritical(proofNetworkJdfDataLog) << "ComponentLink not created.";
+                    return ResourceLinkPoolSP();
+                }
+                components.append(component);
+            } else if (xmlReader.name() == "CuttingParams") {
+                CuttingParamsLinkSP cuttingParams = CuttingParamsLink::fromJdf(xmlReader);
+                if (!cuttingParams) {
+                    qCCritical(proofNetworkJdfDataLog) << "CuttingParamsLink not created.";
+                    return ResourceLinkPoolSP();
+                }
+                linkPool->setCuttingParamsLink(cuttingParams);
+            } else if (xmlReader.name() == "FoldingParamsLink") {
+                FoldingParamsLinkSP foldingParams = FoldingParamsLink::fromJdf(xmlReader);
+                if (!foldingParams) {
+                    qCCritical(proofNetworkJdfDataLog) << "FoldingParamsLink not created.";
+                    return ResourceLinkPoolSP();
+                }
+                linkPool->setFoldingParamsLink(foldingParams);
+            } else {
+                xmlReader.skipCurrentElement();
+            }
+        } else if (xmlReader.isEndElement()) {
+            break;
+        }
+        xmlReader.readNext();
+    }
+    linkPool->setComponentLinks(components);
+
+    return linkPool;
+}
+
+void ResourceLinkPool::toJdf(QXmlStreamWriter &jdfWriter)
+{
+    Q_D(ResourceLinkPool);
+
+    jdfWriter.writeStartElement("ResourceLinkPool");
+
+    for (const ComponentLinkSP &component : d->componentLinks) {
+        if (component)
+            component->toJdf(jdfWriter);
+    }
+    if (d->mediaLink)
+        d->mediaLink->toJdf(jdfWriter);
+    if (d->laminatingIntentLink)
+        d->laminatingIntentLink->toJdf(jdfWriter);
+    if (d->cuttingParamsLink)
+        d->cuttingParamsLink->toJdf(jdfWriter);
+    if (d->foldingParamsLink)
+        d->foldingParamsLink->toJdf(jdfWriter);
+
+    jdfWriter.writeEndElement();
+}
+
+ResourceLinkPoolSP ResourceLinkPool::defaultObject()
+{
+    static ResourceLinkPoolSP entity = create();
+    return entity;
+}
+
+ResourceLinkPool::ResourceLinkPool() :
+    NetworkDataEntity(*new ResourceLinkPoolPrivate)
+{
+}
+
+
+void ResourceLinkPoolPrivate::updateFrom(const NetworkDataEntitySP &other)
+{
+    Q_Q(ResourceLinkPool);
+    ResourceLinkPoolSP castedOther = qSharedPointerCast<ResourceLinkPool>(other);
+    q->setComponentLinks(castedOther->componentLinks());
+    q->setCuttingParamsLink(castedOther->cuttingParamsLink());
+    q->setMediaLink(castedOther->mediaLink());
+    q->setFoldingParamsLink(castedOther->foldingParamsLink());
+
+    NetworkDataEntityPrivate::updateFrom(other);
+
+}
