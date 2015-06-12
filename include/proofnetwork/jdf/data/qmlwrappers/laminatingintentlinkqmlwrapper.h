a8e28aedb7277a564361fd3cd094799e02af8733
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-12 17:20:30 +0300

    LaminatingIntentLinkQmlWrapper class added

---
+++ include/proofnetwork/jdf/data/qmlwrappers/laminatingintentlinkqmlwrapper.h
@@ -0,0 +1,24 @@
+#ifndef LAMINATINGINTENTLINKQMLWRAPPER_H
+#define LAMINATINGINTENTLINKQMLWRAPPER_H
+
+#include "proofnetwork/jdf/data/qmlwrappers/abstractresourcelinkqmlwrapper.h"
+#include "proofnetwork/jdf/proofnetworkjdf_types.h"
+#include "proofnetwork/jdf/proofnetworkjdf_global.h"
+
+namespace Proof {
+namespace Jdf {
+
+class LaminatingIntentLinkQmlWrapperPrivate;
+class LaminatingIntentLinkQmlWrapper : public AbstractResourceLinkQmlWrapper
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(LaminatingIntentLinkQmlWrapper)
+public:
+    explicit LaminatingIntentLinkQmlWrapper(const LaminatingIntentLinkSP &laminating,
+                                            QObject *parent = 0);
+    PROOF_NDE_WRAPPER_TOOLS(LaminatingIntentLink)
+};
+
+}
+}
+#endif // LAMINATINGINTENTLINKQMLWRAPPER_H
---
+++ src/proofnetwork/jdf/data/qmlwrappers/laminatingintentlinkqmlwrapper.cpp
@@ -0,0 +1,21 @@
+#include "laminatingintentlinkqmlwrapper.h"
+#include "proofnetwork/jdf/data/laminatingintentlink.h"
+#include "proofnetwork/jdf/data/qmlwrappers/abstractresourcelinkqmlwrapper_p.h"
+
+namespace Proof {
+namespace Jdf {
+
+class LaminatingIntentLinkQmlWrapperPrivate : public AbstractResourceLinkQmlWrapperPrivate
+{
+    Q_DECLARE_PUBLIC(LaminatingIntentLinkQmlWrapper)
+};
+
+LaminatingIntentLinkQmlWrapper::LaminatingIntentLinkQmlWrapper(const LaminatingIntentLinkSP &laminating, QObject *parent) :
+    AbstractResourceLinkQmlWrapper(laminating, *new  LaminatingIntentLinkQmlWrapperPrivate, parent)
+{
+}
+
+PROOF_NDE_WRAPPER_TOOLS_IMPL(LaminatingIntentLink)
+
+}
+}
