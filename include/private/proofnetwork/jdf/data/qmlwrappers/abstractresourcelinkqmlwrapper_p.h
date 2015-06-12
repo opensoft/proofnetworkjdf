d61bf5a56deb76e7604000fc629ad7c701b5fc44
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-12 13:28:09 +0300

    AbstractResourceLinkQmlWrapperPrivate class added

---
+++ include/private/proofnetwork/jdf/data/qmlwrappers/abstractresourcelinkqmlwrapper_p.h
@@ -0,0 +1,17 @@
+#ifndef ABSTRACTRESOURCELINKQMLWRAPPER_P
+#define ABSTRACTRESOURCELINKQMLWRAPPER_P
+
+#include "proofnetwork/qmlwrappers/networkdataentityqmlwrapper_p.h"
+
+namespace Proof {
+namespace Jdf {
+
+class AbstractResourceLinkQmlWrapperPrivate : public NetworkDataEntityQmlWrapperPrivate
+{
+    Q_DECLARE_PUBLIC(AbstractResourceLinkQmlWrapper)
+};
+
+}
+}
+
+#endif // ABSTRACTRESOURCELINKQMLWRAPPER_P
