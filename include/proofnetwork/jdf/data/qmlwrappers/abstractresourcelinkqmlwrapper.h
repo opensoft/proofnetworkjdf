9a7e91673d0558befe0e1d5eeed4340ae182dd52
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-12 13:27:29 +0300

    QML wrapper for AbstractResourceLink added

---
+++ include/proofnetwork/jdf/data/qmlwrappers/abstractresourcelinkqmlwrapper.h
@@ -0,0 +1,36 @@
+#ifndef ABSTRACTRESOURCELINKQMLWRAPPER_H
+#define ABSTRACTRESOURCELINKQMLWRAPPER_H
+
+#include "proofnetwork/qmlwrappers/networkdataentityqmlwrapper.h"
+#include "proofnetwork/jdf/proofnetworkjdf_types.h"
+#include "proofnetwork/jdf/proofnetworkjdf_global.h"
+#include "proofnetwork/jdf/apihelper.h"
+
+namespace Proof {
+namespace Jdf {
+
+class AbstractResourceLinkQmlWrapperPrivate;
+class AbstractResourceLinkQmlWrapper : public NetworkDataEntityQmlWrapper
+{
+    Q_OBJECT
+    Q_PROPERTY(Proof::Jdf::ApiHelper::Usage usage READ usage NOTIFY usageChanged)
+    Q_PROPERTY(QString rRef READ rRef NOTIFY rRefChanged)
+    Q_DECLARE_PRIVATE(AbstractResourceLinkQmlWrapper)
+public:
+    explicit AbstractResourceLinkQmlWrapper(const QSharedPointer<AbstractResourceLink> &link,
+                                            AbstractResourceLinkQmlWrapperPrivate &dd,
+                                            QObject *parent = 0);
+    ~AbstractResourceLinkQmlWrapper();
+    Proof::Jdf::ApiHelper::Usage usage() const;
+    QString rRef() const;
+protected:
+    void setupEntity(const QSharedPointer<NetworkDataEntity> &old = QSharedPointer<NetworkDataEntity>()) override;
+signals:
+    void usageChanged(Proof::Jdf::ApiHelper::Usage arg);
+    void rRefChanged(QString arg);
+};
+
+}
+}
+
+#endif // ABSTRACTRESOURCELINKQMLWRAPPER_H
---
+++ src/proofnetwork/jdf/data/qmlwrappers/abstractresourcelinkqmlwrapper.cpp
@@ -0,0 +1,44 @@
+#include "abstractresourcelinkqmlwrapper.h"
+#include "proofnetwork/jdf/data/abstractresourcelink.h"
+#include "proofnetwork/jdf/data/qmlwrappers/abstractresourcelinkqmlwrapper_p.h"
+
+
+using namespace Proof::Jdf;
+
+AbstractResourceLinkQmlWrapper::AbstractResourceLinkQmlWrapper(
+        const QSharedPointer<AbstractResourceLink> &link,
+        AbstractResourceLinkQmlWrapperPrivate &dd,
+        QObject *parent)
+    : NetworkDataEntityQmlWrapper(link, dd, parent)
+{
+    setupEntity();
+}
+
+AbstractResourceLinkQmlWrapper::~AbstractResourceLinkQmlWrapper()
+{
+}
+
+PROOF_NDE_WRAPPER_PROPERTY_IMPL_R(AbstractResourceLink, QString, rRef)
+PROOF_NDE_WRAPPER_PROPERTY_IMPL_R(AbstractResourceLink, Proof::Jdf::ApiHelper::Usage, usage)
+
+void AbstractResourceLinkQmlWrapper::setupEntity(const QSharedPointer<Proof::NetworkDataEntity> &old)
+{
+    Q_D(AbstractResourceLinkQmlWrapper);
+    AbstractResourceLinkSP abstractResourceLink = d->entity<AbstractResourceLink>();
+    Q_ASSERT(abstractResourceLink);
+
+    connect(abstractResourceLink.data(), &AbstractResourceLink::rRefChanged,
+            this, &AbstractResourceLinkQmlWrapper::rRefChanged);
+    connect(abstractResourceLink.data(), &AbstractResourceLink::usageChanged,
+            this, &AbstractResourceLinkQmlWrapper::usageChanged);
+
+    AbstractResourceLinkSP castedOld = qSharedPointerCast<AbstractResourceLink>(old);
+    if (castedOld) {
+        if (abstractResourceLink->rRef() != castedOld->rRef())
+            emit rRefChanged(abstractResourceLink->rRef());
+
+        if (abstractResourceLink->usage() != castedOld->usage())
+            emit usageChanged(abstractResourceLink->usage());
+    }
+}
+
