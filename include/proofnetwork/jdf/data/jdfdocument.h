ada78f1a15b59ca09c21215c0a7d13ef7fb8aead
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-12 20:46:55 +0300

    ResourceLinkPoolTest added to JdfDocument

--- include/proofnetwork/jdf/data/jdfdocument.h
+++ include/proofnetwork/jdf/data/jdfdocument.h
@@ -13,6 +13,18 @@ class PROOF_NETWORK_JDF_EXPORT JdfDocument : public JdfNode
     Q_OBJECT
     Q_DECLARE_PRIVATE(JdfDocument)
 public:
+    QString id() const;
+    QString jobId() const;
+    QString jobPartId() const;
+    ResourcePoolSP resourcePool() const;
+    ResourceLinkPoolSP resourceLinkPool() const;
+
+    void setId(const QString &arg);
+    void setJobId(const QString &arg);
+    void setJobPartId(const QString &arg);
+    void setResourcePool(const ResourcePoolSP &arg);
+    void setResourceLinkPool(const ResourceLinkPoolSP &arg);
+
     JdfDocumentQmlWrapper *toQmlWrapper(QObject *parent = 0) const override;
 
     static JdfDocumentSP create();
@@ -21,6 +33,12 @@ public:
     QString toJdf();
     static JdfDocumentSP defaultObject();
 
+signals:
+    void idChanged(const QString &arg);
+    void jobIdChanged(const QString &arg);
+    void jobPartIdChanged(const QString &arg);
+    void resourcePoolChanged(Proof::Jdf::ResourcePoolSP arg);
+    void resourceLinkPoolChanged(Proof::Jdf::ResourceLinkPoolSP arg);
 protected:
     explicit JdfDocument();
 };
--- src/proofnetwork/jdf/data/jdfdocument.cpp
+++ src/proofnetwork/jdf/data/jdfdocument.cpp
@@ -1,11 +1,23 @@
 #include "jdfdocument.h"
 #include "proofnetwork/networkdataentity_p.h"
+#include "proofnetwork/jdf/data/resourcepool.h"
+#include "proofnetwork/jdf/data/resourcelinkpool.h"
+#include "proofnetwork/jdf/data/qmlwrappers/jdfdocumentqmlwrapper.h"
+
 namespace Proof {
 namespace Jdf {
 
 class JdfDocumentPrivate : public NetworkDataEntityPrivate
 {
     Q_DECLARE_PUBLIC(JdfDocument)
+
+    void updateFrom(const Proof::NetworkDataEntitySP &other) override;
+
+    QString id;
+    QString jobId;
+    QString jobPartId;
+    ResourcePoolSP resourcePool = ResourcePool::defaultObject();
+    ResourceLinkPoolSP resourceLinkPool = ResourceLinkPool::defaultObject();
 };
 
 }
@@ -13,8 +25,77 @@ class JdfDocumentPrivate : public NetworkDataEntityPrivate
 
 using namespace Proof::Jdf;
 
-JdfDocument::JdfDocument()
+QString JdfDocument::id() const
 {
+    Q_D(const JdfDocument);
+    return d->id;
+}
+
+QString JdfDocument::jobId() const
+{
+    Q_D(const JdfDocument);
+    return d->jobId;
+}
+
+QString JdfDocument::jobPartId() const
+{
+    Q_D(const JdfDocument);
+    return d->jobPartId;
+}
+
+ResourcePoolSP JdfDocument::resourcePool() const
+{
+    Q_D(const JdfDocument);
+    return d->resourcePool;
+}
+
+ResourceLinkPoolSP JdfDocument::resourceLinkPool() const
+{
+    Q_D(const JdfDocument);
+    return d->resourceLinkPool;
+}
+
+void JdfDocument::setId(const QString &arg)
+{
+    Q_D(JdfDocument);
+    if (d->id != arg) {
+        d->id = arg;
+        emit idChanged(d->id);
+    }
+}
+
+void JdfDocument::setJobId(const QString &arg)
+{
+    Q_D(JdfDocument);
+    if (d->jobId != arg) {
+        d->jobId = arg;
+        emit jobIdChanged(d->jobId);
+    }
+}
+
+void JdfDocument::setJobPartId(const QString &arg)
+{
+    Q_D(JdfDocument);
+    if (d->jobPartId != arg) {
+        d->jobPartId = arg;
+        emit jobPartIdChanged(d->jobPartId);
+    }
+}
+
+void JdfDocument::setResourcePool(const ResourcePoolSP &arg)
+>>>>>>> e8d0b4ce... ResourceLinkPoolTest added to JdfDocument
+{
+}
+
+void JdfDocument::setResourceLinkPool(const ResourceLinkPoolSP &arg)
+{
+    Q_D(JdfDocument);
+    if (arg == nullptr)
+        setResourceLinkPool(ResourceLinkPool::defaultObject());
+    else if (d->resourceLinkPool != arg) {
+        d->resourceLinkPool = arg;
+        emit resourceLinkPoolChanged(d->resourceLinkPool);
+    }
 }
 
 JdfDocumentQmlWrapper *JdfDocument::toQmlWrapper(QObject *parent) const
@@ -35,8 +116,30 @@ JdfDocumentSP JdfDocument::create()
 JdfDocumentSP JdfDocument::fromJdf(QXmlStreamReader &xmlReader)
 {
     JdfDocumentSP document = create();
-    JdfNodeSP node = JdfNode::fromJdf(xmlReader);
-    document->updateFrom(node);
+    document->setFetched(true);
+
+    while (!xmlReader.atEnd() && !xmlReader.hasError()) {
+        QXmlStreamReader::TokenType token = xmlReader.readNext();
+        if (token == QXmlStreamReader::StartDocument)
+            continue;
+        if (token == QXmlStreamReader::StartElement) {
+            if (xmlReader.name() == "JDF") {
+                QXmlStreamAttributes attributes = xmlReader.attributes();
+                if (attributes.value("Type").toString() == "Product") {
+                    document->setId(attributes.value("ID").toString());
+                    document->setJobId(attributes.value("JobID").toString());
+                    document->setJobPartId(attributes.value("JobPartID").toString());
+                }
+            }
+
+            if (xmlReader.name() == "ResourcePool")
+                document->setResourcePool(ResourcePool::fromJdf(xmlReader, document->id()));
+
+            if (xmlReader.name() == "ResourceLinkPool")
+                document->setResourceLinkPool(ResourceLinkPool::fromJdf(xmlReader));
+        }
+    }
+
     return document;
 }
 
@@ -49,7 +152,19 @@ QString JdfDocument::toJdf()
     jdfWriter.writeStartDocument();
     jdfWriter.writeDefaultNamespace("http://www.CIP4.org/JDFSchema_1_1");
 
-    JdfNode::toJdf(jdfWriter);
+    jdfWriter.writeStartElement("JDF");
+    {
+        jdfWriter.writeDefaultNamespace("http://www.CIP4.org/JDFSchema_1_1");
+        jdfWriter.writeAttribute("ID", d->id);
+        jdfWriter.writeAttribute("JobID", d->jobId);
+        jdfWriter.writeAttribute("JobPartID", d->jobPartId);
+        jdfWriter.writeAttribute("Status", "Waiting");
+        jdfWriter.writeAttribute("Type", "Product");
+        jdfWriter.writeAttribute("Version", "1.4");
+        d->resourcePool->toJdf(jdfWriter);
+        d->resourceLinkPool->toJdf(jdfWriter);
+    }
+    jdfWriter.writeEndElement();
 
     jdfWriter.writeEndDocument();
 
