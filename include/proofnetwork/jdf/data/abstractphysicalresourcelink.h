ccfb191feae8ae75435c0c35850426fb6c511593
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-17 13:39:55 +0300

    AbstractPhysicalResourceLink class added

---
+++ include/proofnetwork/jdf/data/abstractphysicalresourcelink.h
@@ -0,0 +1,11 @@
+#ifndef ABSTRACTPHYSICALRESOURCELINK_H
+#define ABSTRACTPHYSICALRESOURCELINK_H
+
+
+class AbstractPhysicalResourceLink : public AbstractResourceLink
+{
+public:
+    AbstractPhysicalResourceLink();
+};
+
+#endif // ABSTRACTPHYSICALRESOURCELINK_H
---
+++ src/proofnetwork/jdf/data/abstractphysicalresourcelink.cpp
@@ -0,0 +1,7 @@
+#include "abstractphysicalresourcelink.h"
+
+AbstractPhysicalResourceLink::AbstractPhysicalResourceLink()
+{
+
+}
+
