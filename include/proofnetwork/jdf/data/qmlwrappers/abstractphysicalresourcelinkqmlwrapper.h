c924230db136bdda6dd4cca174f029ba7b483f66
Sergey Platonov <sergey.platonov@opensoftdev.ru>
2015-06-18 01:34:09 +0300

    AbstractPhysicalResourceLinkQmlWrapper and AbstractPhysicalResourceLinkQmlWrapperPrivate classes added

---
+++ include/proofnetwork/jdf/data/qmlwrappers/abstractphysicalresourcelinkqmlwrapper.h
@@ -0,0 +1,12 @@
+#ifndef ABSTRACTPHYSICALRESOURCELINKQMLWRAPPER_H
+#define ABSTRACTPHYSICALRESOURCELINKQMLWRAPPER_H
+
+#include <QObject>
+
+class AbstractPhysicalResourceLinkQmlWrapper : public AbstractResourceLinkQmlWrapper
+{
+public:
+    AbstractPhysicalResourceLinkQmlWrapper();
+};
+
+#endif // ABSTRACTPHYSICALRESOURCELINKQMLWRAPPER_H
---
+++ src/proofnetwork/jdf/data/qmlwrappers/abstractphysicalresourcelinkqmlwrapper.cpp
@@ -0,0 +1,7 @@
+#include "abstractphysicalresourcelinkqmlwrapper.h"
+
+AbstractPhysicalResourceLinkQmlWrapper::AbstractPhysicalResourceLinkQmlWrapper()
+{
+
+}
+
